7b7c6941a8519b1f92aa8b705227ebc0aa209b1e
Graceey09 <gracemaduekwe95@gmail.com>
2023-11-16 15:31:47 +0100

    first commit

---
+++ shoppingList.js
@@ -0,0 +1,50 @@
+function createRowElements(){
+    return {
+      shoppingCart: document.getElementsByTagName("tbody")[0],
+      newRow: document.createElement("tr"),
+      newItem: document.createElement("td"),
+      itemPrice: document.createElement("td"),
+      removeButton: document.createElement("button")
+    }
+  }
+  function getUserInput(){
+    item = document.getElementById("item");
+    price = document.getElementById("price");
+    return {
+      item, price
+    }
+  }
+  
+  
+  function addToCart(){
+    let {item, price} =  getUserInput();
+  
+    if(item.value !== "" && price.value !== ""){
+      let {newRow, shoppingCart, newItem, itemPrice, removeButton} = createRowElements();
+      newItem.innerText = item.value;
+      itemPrice.innerText = price.value;
+  
+      // removeButton.innerText = "Remove";
+      // removeButton.onclick = function() {
+      //   shoppingCart.removeChild(newRow);
+      
+  
+      newRow.appendChild(newItem)
+      newRow.appendChild(itemPrice)
+      shoppingCart.appendChild(newRow)
+  
+      item.value = "";
+      price.value = "";
+    }
+  
+  }
+  
+  function removeFromCart(){
+    const shoppingCart = document.getElementsByTagName("tbody")[0];
+    const rows = shoppingCart.getElementsByTagName("tr");
+  
+    if(rows.length >0){
+      shoppingCart.removeChild(rows[rows.length-1]);
+  }
+  }
+  
